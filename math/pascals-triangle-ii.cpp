e067106155f3942d39904281a7580d3556deca97
doudou <531047675@qq.com>
2016-03-16 23:40:05 +0800

    ADDED Pascal Triangle II git add math/ README.md

---
+++ math/pascals-triangle-ii.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+class Solution{
+public:
+	vector<int> getRow(int rowIndex){
+		vector<int> res(rowIndex+1,1);
+		if(rowIndex==0 || rowIndex==1)
+			return res;
+		int i,j;
+		for(i=2;i<=rowIndex;++i)
+			for(j=i-1;j>=1;--j){
+				res[j] = res[j-1] = res[j];
+			}
+
+		return res;
+	}
+};
+
+int main()
+{
+	return 0;
+}
